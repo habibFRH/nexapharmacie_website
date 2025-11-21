5dd0f14b627df5e4bdd6a5a00e3eeb9238b845cd
habibFrh <habibellah.ferhi@univ-constantone2.dz>
2025-11-21 11:46:09 +0100

    add embeded youtube link

--- src/pages/ScientistsPage.tsx
+++ src/pages/ScientistsPage.tsx
@@ -569,7 +569,7 @@ const ScientistsPage = () => {
           <div className="relative pb-[56.25%] h-0 overflow-hidden rounded-2xl shadow-lg">
             <iframe
               className="absolute top-0 left-0 w-full h-full"
-              src="https://www.youtube.com/watch?v=RLwrC0lS6Ek"
+              src="https://www.youtube.com/embed/RLwrC0lS6Ek"
               title="YouTube video"
               frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
